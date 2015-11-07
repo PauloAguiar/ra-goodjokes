3f2f2ca4adc6fe61403822cac033b9e43ab35c31
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2015-11-07 17:04:44 -0200

    Added morgan logger Added bootstrap basic setup with EJS engine.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan')
 
 app.set('view engine', 'ejs');
+app.use(morgan('dev'))
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {'title': 'Good Jokes Mate'});
 });
 
 var server = app.listen(3000, function () {
