179d3f2a67ee4646f1f67154497a37adba0badcf
Matheus Camargo <matheuscamargo@gmail.com>
2015-11-07 19:20:17 -0200

    Refactoring and creating routes

---
+++ config/express.js
@@ -0,0 +1,10 @@
+var morgan = require('morgan');
+var express = require('express');
+
+module.exports = function(app) {
+	app.set('view engine', 'ejs');
+	app.use(morgan('dev'))
+	console.log(__dirname);
+	app.use('/bower_components',  express.static(__dirname + '/../bower_components'));
+	app.use('/public',  express.static(__dirname + '/../public'));
+}
