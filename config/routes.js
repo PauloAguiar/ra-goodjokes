4b3f21cccf413fa23a2fcc2e5446f75c345ec2be
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2015-11-08 01:12:06 -0200

    Added jquery ajax queries and sample data on server-side. Added EJS engine on client-side.

--- config/routes.js
+++ config/routes.js
@@ -12,6 +12,67 @@ module.exports = function (app) {
         res.render('statistics', {'title': 'Good Jokes Mate'});
     });
 
+    app.get('/questionsSample', function (req, res) {
+        res.json(
+            [
+                {
+                    'answers': 0,
+                    'views': 550,
+                    'votes': 2,
+                    'text': 'Good Jokes Mate',
+                    'tags': ['good', 'jokes', 'mate'],
+                    'vest': ['ITA 2015'],
+                    'date': 1446950406,
+                    'user': 'alexandremuzio'
+                },
+                {
+                    'answers': 11,
+                    'views': 2000,
+                    'votes': 0,
+                    'text': 'Good Jokes Mate 2',
+                    'tags': ['real', 'funny'],
+                    'date': 1446949866,
+                    'user': 'johngarden'
+                },
+                {
+                    'answers': 11,
+                    'views': 2000,
+                    'votes': 0,
+                    'text': 'Good Jokes Mate 3',
+                    'tags': ['real', 'funny'],
+                    'vest': ['ITA 2015'],
+                    'date': 1446949866,
+                    'user': 'mateus'
+                }
+            ]);
+    });
+
+    app.get('/questionsSample/ita-2015', function (req, res) {
+        res.json(
+            [
+                {
+                    'answers': 0,
+                    'views': 550,
+                    'votes': 2,
+                    'text': 'Good Jokes Mate',
+                    'tags': ['good', 'jokes', 'mate'],
+                    'vest': ['ITA 2015'],
+                    'date': 1446950406,
+                    'user': 'alexandremuzio'
+                },
+                {
+                    'answers': 11,
+                    'views': 2000,
+                    'votes': 0,
+                    'text': 'Good Jokes Mate 3',
+                    'tags': ['real', 'funny'],
+                    'vest': ['ITA 2015'],
+                    'date': 1446949866,
+                    'user': 'mateus'
+                }
+            ]);
+    });
+
 	app.param ('userId',        user.load);
 	app.get   ('/users/:id',    user.show);
 	app.get   ('/users/create', user.create);
