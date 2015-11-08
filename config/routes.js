c55b684ae14fd04e5544731dc29ae90ea72b7693
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2015-11-07 22:25:00 -0200

    Updated front page.

--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,8 @@ module.exports = function (app) {
 	app.get('/', function (req, res) {
 		res.render('index', {'title': 'Good Jokes Mate'});
 	});
+
+    app.get('/statistics', function (req, res) {
+        res.render('statistics', {'title': 'Good Jokes Mate'});
+    });
 }
